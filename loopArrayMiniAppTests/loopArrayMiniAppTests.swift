8c78c82bd59e2ea59f96c05576b8e7865ed8897f
Nick VIntartas <nv2009123@gmail.com>
2024-10-16 11:11:34 -0500

    Initial Commit

---
+++ loopArrayMiniAppTests/loopArrayMiniAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  loopArrayMiniAppTests.swift
+//  loopArrayMiniAppTests
+//
+//  Created by NICHOLAS VINTARTAS on 10/16/24.
+//
+
+import Testing
+@testable import loopArrayMiniApp
+
+struct loopArrayMiniAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ loopArrayMiniAppUITests/loopArrayMiniAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  loopArrayMiniAppUITestsLaunchTests.swift
+//  loopArrayMiniAppUITests
+//
+//  Created by NICHOLAS VINTARTAS on 10/16/24.
+//
+
+import XCTest
+
+final class loopArrayMiniAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
